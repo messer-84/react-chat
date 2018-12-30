b63b02673b6ab87a199a4600e58a38d3eedf75a9
maksimvorobyov <gtl.vorobyov@gmail.com>
2018-12-30 19:03:27 +0200

    clean app chat

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React, {Component} from 'react';
-import './App.css';
-
-import Welcome from './Welcome';
-import Clock from './Clock';
-import List from './List';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Welcome name="World">World</Welcome>
-          <hr/>
-          <Clock/>
-          <List/>
-        </header>
+      <div>
+        <h1>Chat</h1>
       </div>
     );
   }
